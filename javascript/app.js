2b34a25fdb4c532d3a8cb341a689644c4ce99bc5
RobertsOpemipo <108809044+RobertsOpemipo@users.noreply.github.com>
2023-08-03 21:15:12 +0100

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+const signUpButton = document.getElementById('signUp');
+const logInButton = document.getElementById('logIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+logInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
+
+})
